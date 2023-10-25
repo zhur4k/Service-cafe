135fc6a8da2f8699ebdb8f56bf315627d26e168f
kostya1205 <kostya.zhurovich12052@gmail.com>
2023-10-25 21:54:51 +0300

    Add CashRegisterService

--- src/main/java/com/yulkost/service/config/WebSecurityConfig.java
+++ src/main/java/com/yulkost/service/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,7 +20,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/**","/error").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
---
+++ src/main/java/com/yulkost/service/service/CashRegisterService.java
@@ -0,0 +1,35 @@
+package com.yulkost.service.service;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class CashRegisterService {
+
+    private final String CASH_REGISTER_ID ="http://169.254.35.154" ;
+    public String SendCheck(){
+        String json = "{\"F\":[{\"C\":{\"cm\":\"Кассир: Светлана\"}}" +
+                ",{\"S\":{\"code\":\"1234567890121\",\"name\":\"Конфета\",\"qty\":\"1.000\"\"price\":\"0.01\",\"sum\":\"0.01\"}}" +
+                ",{\"P\":{}}]}";
+
+        return SendJson(json, "/cgi/chk");
+    }
+
+    private String SendJson(String json,String url){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // Создаем HttpEntity с JSON-чеком и заголовками
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
+
+        // Отправляем POST-запрос на сервер
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(CASH_REGISTER_ID +url, request, String.class);
+        System.out.println("Ответ от сервера: " + response.getBody());
+        return "Запрос успешно выполнен!";
+    }
+}
