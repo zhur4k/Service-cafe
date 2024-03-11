86b7d01092478624b6639fc1fd8a6e5a6c710ae4
Kostya1205 <kostya.zhurovich12052@gmail.com>
2024-03-11 13:21:19 +0300

    Second version of menu

--- src/main/resources/static/js/Menu.js
+++ src/main/resources/static/js/Menu.js
@@ -3,29 +3,35 @@ getCategories();
 
 function displayMenu() {
     let bodyElement = document.getElementById("main_body");
-
+    let categoryMain = document.createElement("div");
+    categoryMain.classList.add("main-category"); // Добавляем класс "category" для стилизации
     categories.forEach(category => {
-        bodyElement.appendChild(displayCategory(category));
+        categoryMain.appendChild(displayCategory(category));
     });
-
+    bodyElement.appendChild(categoryMain);
 }
-function displayCategory(category){
-    let categoryItem = document.createElement("li");
-    categoryItem.textContent = category.categoriesName;
+
+function displayCategory(category) {
+    let categoryItem = document.createElement("div");
+    categoryItem.classList.add("category"); // Добавляем класс "category" для стилизации
+
+    let categoryName = document.createElement("div");
+    categoryName.textContent = category.categoriesName;
+    categoryName.classList.add("category-name"); // Добавляем класс "category-name" для стилизации
+    categoryItem.appendChild(categoryName);
 
     let itemsList = document.createElement("ul");
     category.items.forEach(item => {
-        // Создаем элемент списка для товара
         let itemElement = document.createElement("li");
-        itemElement.textContent = `${item.nameOfItems} - ${(item.price/100).toFixed(2)} рублей`;
-
-        // Добавляем товар в подсписок
+        itemElement.innerHTML = `<span class="item-name">${item.nameOfItems}</span> <span class="item-price">${(item.price / 100).toFixed(2)}</span>`;
         itemsList.appendChild(itemElement);
     });
     categoryItem.appendChild(itemsList);
-    category.childCategories.forEach(category => {
-        categoryItem.appendChild(displayCategory(category))
+
+    category.childCategories.forEach(childCategory => {
+        categoryItem.appendChild(displayCategory(childCategory));
     });
+
     return categoryItem;
 }
 function getCategories() {
