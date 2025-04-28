534ddf261d7c31068b6c3197221050bf5ae0af01
krilop <krilop.gamer@gmail.com>
2025-04-28 16:16:30 +0300

    Translation of dumps and application configuration change
    Migrated database dumps from MySQL to PostgreSQL and updated the application's configuration.
    
    Note: There is an issue with Flyway during application startup related to PostgreSQL configuration. Further investigation is required to resolve it.

--- src/main/resources/migrations/V1__servise_db.sql
+++ src/main/resources/migrations/V1__servise_db.sql
@@ -1,200 +1,155 @@
-create table categories
-(
-    id                 bigint auto_increment
-        primary key,
-    categories_name    varchar(255) null,
-    parent_category_id bigint       null,
-    constraint FK_parent_category_category
-        foreign key (parent_category_id) references categories (id)
+-- Удаление всех таблиц (если надо)
+DROP TABLE IF EXISTS user_role, shift_users, users, product_weight, items_in_item, items, units, product_stock_movement, order_items, cash_register, orders, collections, shift, product_stock, products, categories CASCADE;
+
+-- Создание ENUM'а для user_role
+CREATE TYPE user_role_enum AS ENUM ('ACCOUNTANT', 'ADMIN', 'CASHIER');
+
+-- Теперь создание таблиц
+
+CREATE TABLE categories (
+                            id BIGSERIAL PRIMARY KEY,
+                            categories_name VARCHAR(255),
+                            parent_category_id BIGINT,
+                            CONSTRAINT fk_parent_category_category FOREIGN KEY (parent_category_id) REFERENCES categories (id)
 );
 
-create table products
-(
-    id               bigint auto_increment
-        primary key,
-    name             varchar(255) null
+CREATE TABLE products (
+                          id BIGSERIAL PRIMARY KEY,
+                          name VARCHAR(255)
 );
 
-create table product_stock
-(
-    id         bigint auto_increment
-        primary key,
-    price      int    not null,
-    weight     int    not null,
-    product_id bigint null,
-    constraint FK_stock_products_product
-        foreign key (product_id) references products(id)
+CREATE TABLE product_stock (
+                               id BIGSERIAL PRIMARY KEY,
+                               price INT NOT NULL,
+                               weight INT NOT NULL,
+                               product_id BIGINT,
+                               CONSTRAINT fk_stock_products_product FOREIGN KEY (product_id) REFERENCES products (id)
 );
 
-
-create table shift
-(
-    id             bigint auto_increment
-        primary key,
-    end_date       datetime(6) null,
-    start_date     datetime(6) null,
-    state_of_shift bit         not null
+CREATE TABLE shift (
+                       id BIGSERIAL PRIMARY KEY,
+                       end_date TIMESTAMP(6),
+                       start_date TIMESTAMP(6),
+                       state_of_shift BOOLEAN NOT NULL
 );
 
-create table collections
-(
-    id                bigint auto_increment
-        primary key,
-    date_of_operation datetime(6) null,
-    sum_of_operation  int         null,
-    type_of_operation bit         null,
-    shift_id          bigint      null,
-    constraint FK_shift_collections
-        foreign key (shift_id) references shift (id)
+CREATE TABLE collections (
+                             id BIGSERIAL PRIMARY KEY,
+                             date_of_operation TIMESTAMP(6),
+                             sum_of_operation INT,
+                             type_of_operation BOOLEAN,
+                             shift_id BIGINT,
+                             CONSTRAINT fk_shift_collections FOREIGN KEY (shift_id) REFERENCES shift (id)
 );
 
-create table orders
-(
-    id                 bigint auto_increment
-        primary key,
-    cash_less_paid     int         not null,
-    cash_paid          int         not null,
-    date               datetime(6) null,
-    establishment_paid int         not null,
-    number_of_table    int         not null,
-    sum_of_change      int         not null,
-    shift_id           bigint      null,
-    constraint FK_shift_orders
-        foreign key (shift_id) references shift (id)
+CREATE TABLE orders (
+                        id BIGSERIAL PRIMARY KEY,
+                        cash_less_paid INT NOT NULL,
+                        cash_paid INT NOT NULL,
+                        date TIMESTAMP(6),
+                        establishment_paid INT NOT NULL,
+                        number_of_table INT NOT NULL,
+                        sum_of_change INT NOT NULL,
+                        shift_id BIGINT,
+                        CONSTRAINT fk_shift_orders FOREIGN KEY (shift_id) REFERENCES shift (id)
 );
 
-create table cash_register
-(
-    id           bigint auto_increment
-        primary key,
-    cash_amount  int    not null,
-    collection_id bigint null,
-    order_id     bigint null,
-    constraint UK_4b75tsgurx07kopc6pltphe1m
-        unique (collection_id),
-    constraint UK_4kdvi0ogf4ciycan4rbqjd247
-        unique (order_id),
-    constraint FK_collection_cash_register
-        foreign key (collection_id) references collections (id),
-    constraint FK_order_cash_register
-        foreign key (order_id) references orders (id)
+CREATE TABLE cash_register (
+                               id BIGSERIAL PRIMARY KEY,
+                               cash_amount INT NOT NULL,
+                               collection_id BIGINT UNIQUE,
+                               order_id BIGINT UNIQUE,
+                               CONSTRAINT fk_collection_cash_register FOREIGN KEY (collection_id) REFERENCES collections (id),
+                               CONSTRAINT fk_order_cash_register FOREIGN KEY (order_id) REFERENCES orders (id)
 );
 
-create table order_items
-(
-    id                  bigint auto_increment
-        primary key,
-    category            varchar(255) null,
-    date_of_item_change datetime(6)  null,
-    discount            int          not null,
-    item                bigint       null,
-    name_of_items       varchar(255) null,
-    product_volume       varchar(255) null,
-    price               int          not null,
-    quantity            int          not null,
-    type_of_item        bit          null,
-    unit                varchar(255) null,
-    order_id            bigint       null,
-    constraint FK_order_order_items
-        foreign key (order_id) references orders (id)
+CREATE TABLE order_items (
+                             id BIGSERIAL PRIMARY KEY,
+                             category VARCHAR(255),
+                             date_of_item_change TIMESTAMP(6),
+                             discount INT NOT NULL,
+                             item BIGINT,
+                             name_of_items VARCHAR(255),
+                             product_volume VARCHAR(255),
+                             price INT NOT NULL,
+                             quantity INT NOT NULL,
+                             type_of_item BOOLEAN,
+                             unit VARCHAR(255),
+                             order_id BIGINT,
+                             CONSTRAINT fk_order_order_items FOREIGN KEY (order_id) REFERENCES orders (id)
 );
 
-create table product_stock_movement
-(
-    id                bigint auto_increment
-        primary key,
-    balance_weight    int          not null,
-    date_of_operation datetime(6)  null,
-    description       varchar(255) null,
-    price_movement    int          not null,
-    price_on_stock    int          not null,
-    product           bigint       null,
-    product_name      varchar(255) null,
-    type_of_operation varchar(255) null,
-    weight            int          not null,
-    order_items_id    bigint       null,
-    constraint FK_order_items_product_stock_movement
-        foreign key (order_items_id) references order_items (id)
+CREATE TABLE product_stock_movement (
+                                        id BIGSERIAL PRIMARY KEY,
+                                        balance_weight INT NOT NULL,
+                                        date_of_operation TIMESTAMP(6),
+                                        description VARCHAR(255),
+                                        price_movement INT NOT NULL,
+                                        price_on_stock INT NOT NULL,
+                                        product BIGINT,
+                                        product_name VARCHAR(255),
+                                        type_of_operation VARCHAR(255),
+                                        weight INT NOT NULL,
+                                        order_items_id BIGINT,
+                                        CONSTRAINT fk_order_items_product_stock_movement FOREIGN KEY (order_items_id) REFERENCES order_items (id)
 );
 
-create table units
-(
-    id   bigint auto_increment
-        primary key,
-    name varchar(255) null
+CREATE TABLE units (
+                       id BIGSERIAL PRIMARY KEY,
+                       name VARCHAR(255)
 );
 
-create table items
-(
-    id             bigint auto_increment
-        primary key,
-    date_of_change datetime(6)  null,
-    img            varchar(255) null,
-    name_of_items  varchar(255) null,
-    product_volume  varchar(255) null,
-    price          int          not null,
-    type_of_item   bit          null,
-    view           bit          null,
-    categories_id  bigint       null,
-    unit_id        bigint       null,
-    constraint FK_unit_items
-        foreign key (unit_id) references units (id),
-    constraint FK_categories_items
-        foreign key (categories_id) references categories (id)
+CREATE TABLE items (
+                       id BIGSERIAL PRIMARY KEY,
+                       date_of_change TIMESTAMP(6),
+                       img VARCHAR(255),
+                       name_of_items VARCHAR(255),
+                       product_volume VARCHAR(255),
+                       price INT NOT NULL,
+                       type_of_item BOOLEAN,
+                       view BOOLEAN,
+                       categories_id BIGINT,
+                       unit_id BIGINT,
+                       CONSTRAINT fk_unit_items FOREIGN KEY (unit_id) REFERENCES units (id),
+                       CONSTRAINT fk_categories_items FOREIGN KEY (categories_id) REFERENCES categories (id)
 );
 
-create table items_in_item
-(
-    id             bigint auto_increment
-        primary key,
-    quantity       int    not null,
-    item_id        bigint null,
-    parent_item_id bigint null,
-    constraint FK_parent_item_items_in_item
-        foreign key (parent_item_id) references items (id),
-    constraint FK_item_items_in_item
-        foreign key (item_id) references items (id)
+CREATE TABLE items_in_item (
+                               id BIGSERIAL PRIMARY KEY,
+                               quantity INT NOT NULL,
+                               item_id BIGINT,
+                               parent_item_id BIGINT,
+                               CONSTRAINT fk_parent_item_items_in_item FOREIGN KEY (parent_item_id) REFERENCES items (id),
+                               CONSTRAINT fk_item_items_in_item FOREIGN KEY (item_id) REFERENCES items (id)
 );
 
-create table product_weight
-(
-    id         bigint auto_increment
-        primary key,
-    weight     int    not null,
-    item_id    bigint null,
-    product_id bigint null,
-    constraint FK_product_product_weight
-        foreign key (product_id) references products (id),
-    constraint FK_item_product_weight
-        foreign key (item_id) references items (id)
+CREATE TABLE product_weight (
+                                id BIGSERIAL PRIMARY KEY,
+                                weight INT NOT NULL,
+                                item_id BIGINT,
+                                product_id BIGINT,
+                                CONSTRAINT fk_product_product_weight FOREIGN KEY (product_id) REFERENCES products (id),
+                                CONSTRAINT fk_item_product_weight FOREIGN KEY (item_id) REFERENCES items (id)
 );
 
-create table users
-(
-    id       bigint auto_increment
-        primary key,
-    active   bit          not null,
-    chat_id  bigint       not null,
-    login    varchar(255) null,
-    name     varchar(255) null,
-    password varchar(255) null
+CREATE TABLE users (
+                       id BIGSERIAL PRIMARY KEY,
+                       active BOOLEAN NOT NULL,
+                       chat_id BIGINT NOT NULL,
+                       login VARCHAR(255),
+                       name VARCHAR(255),
+                       password VARCHAR(255)
 );
 
-create table shift_users
-(
-    shift_id bigint not null,
-    user_id  bigint not null,
-    constraint FK_shift_user
-        foreign key (user_id) references users (id),
-    constraint FK_user_shift
-        foreign key (shift_id) references shift (id)
+CREATE TABLE shift_users (
+                             shift_id BIGINT NOT NULL,
+                             user_id BIGINT NOT NULL,
+                             CONSTRAINT fk_shift_user FOREIGN KEY (user_id) REFERENCES users (id),
+                             CONSTRAINT fk_user_shift FOREIGN KEY (shift_id) REFERENCES shift (id)
 );
 
-create table user_role
-(
-    user_id bigint                                  not null,
-    roles   enum ('ACCOUNTANT', 'ADMIN', 'CASHIER') null,
-    constraint FK_user_role
-        foreign key (user_id) references users (id)
+CREATE TABLE user_role (
+                           user_id BIGINT NOT NULL,
+                           roles user_role_enum,
+                           CONSTRAINT fk_user_role FOREIGN KEY (user_id) REFERENCES users (id)
 );
-
